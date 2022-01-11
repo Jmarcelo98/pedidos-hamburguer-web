417405efcfef7cb20db4ef7ac0d41512bb9a6873
Joao Marcelo de Jesus Macedo <joaomacedo.millenium@poupex.com.br>
2022-01-11 20:29:15 -0300

    Iniciado frontend de fazer pedido

--- src/app/pages/public/pedidos/pedidos.component.ts
+++ src/app/pages/public/pedidos/pedidos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pedidos',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidosComponent implements OnInit {
 
-  constructor() { }
+  // form do pedido
+  pedidoForm = this.formBuilder.group({
+    alface: [true, [Validators.required]],
+  })
+
+  acrescentar = environment.ACRESCENTAR
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  get f() {
+    return this.pedidoForm?.controls;
+  }
+
+  fazerPedido() {
+
+    console.log(this.f.alface.value);
+
+
   }
 
 }
